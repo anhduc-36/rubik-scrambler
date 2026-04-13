b0c09128a429c16869c9d3a2ccfb92cc54a63b9f
anhduc-36 <anhduc.nguyen.3329@gmail.com>
2026-04-13 22:23:08 +0700

    Update main.cpp
    Move scramble generating to separate function
    Added number of scrambles to generation

--- main.cpp
+++ main.cpp
@@ -6,41 +6,62 @@
 
 using namespace std;
 
-const char moves[6] = {'R', 'L', 'F', 'B', 'U', 'D'};
-const char* modifier[3] = {"", "'", "2"};
+const char moves[6] = { 'R', 'L', 'F', 'B', 'U', 'D' };
+const char* modifier[3] = { "", "'", "2" };
+
+string generateScramble(int length)
+{
+	random_device rd;
+	mt19937 mt(rd());
+	uniform_int_distribution<int> movesdist(0, 5);
+	uniform_int_distribution<int> modifdist(0, 2);
+	string scramble;
+	int lastMove = -1;
+	for (int i = 0; i < length; i++)
+	{
+		int currentMove;
+		// Prevent the same face from moving twice in a row
+		do
+		{
+			currentMove = movesdist(mt);
+		} 
+		while (currentMove == lastMove);
+		scramble += moves[currentMove];
+		scramble += modifier[modifdist(mt)];
+		scramble += " ";
+		lastMove = currentMove;
+	}
+	return scramble;
+}
 
 int main()
 {
-    int sz = 20;
-    random_device rd;
-    mt19937 mt(rd());
-    uniform_int_distribution<int> movesdist(0,5);
-    uniform_int_distribution<int> modifdist(0,2);
+	int scrambleSize = 20;
+	int scrambleCount = 1;
 
-	start:
+start:
 	cout << "Enter your scramble length: ";
-	cin >> sz;
-	if (sz <= 0)
+	cin >> scrambleSize;
+	if (scrambleSize <= 0)
 	{
 		cout << "Invalid scramble! Please try again.\n";
 		goto start;
 	}
-	
-	cout << "Your Scramble: ";
-    
-	int lastMove = -1;
-    for (int i = 0; i < sz; i++)
-    {
-        int currentMove;
+	cout << "Enter number of scramble: ";
+	cin >> scrambleCount;
+	if (scrambleCount <= 0)
+	{
+		cout << "Invalid scramble! Please try again.\n";
+		goto start;
+	}
 
-        // Prevent the same face from moving twice in a row
-        do
-        {
-            currentMove = movesdist(mt);
-        }
-        while (currentMove == lastMove);
-        cout << moves[currentMove] << modifier[modifdist(mt)] << " ";
-        lastMove = currentMove;
-    }
-    return 0;
+	cout << "Your Scramble: \n";
+
+	int lastMove = -1;
+	for (int i = 0; i < scrambleCount; i++)
+	{
+		cout << i + 1 << ". " << generateScramble(scrambleSize) << "\n";
+	}
+
+	return 0;
 }
